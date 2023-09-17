ac7e42e11d0a33e527e4fa9a5eacf3988f551acc
Marko Tikkanen <martikkanen@outlook.com>
2023-09-18 00:37:36 +0300

    .

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -6,19 +6,21 @@ use match_numbers::{
 };
 
 use criterion::{
+    black_box,
     criterion_group,
     criterion_main,
     Criterion,
 };
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let my_vec = generate_vec(10000000, 5643, 967, 40);
+    let my_vec = black_box(generate_vec(100000000, 5643, 967, 40));
     let mut my_vec_2 = my_vec.clone();
     let target: i32 = 6610;
 
     //c.bench_function("find_pair", |b| b.iter(|| find_pair(&my_vec, &target)));
     c.bench_function("find_pair_i", |b| b.iter(|| find_pair_i(&mut my_vec_2, &target)));
     c.bench_function("find_pair_map", |b| b.iter(|| find_pair_map(&my_vec, &target)));
+
 }
 
 criterion_group!(benches, criterion_benchmark);
