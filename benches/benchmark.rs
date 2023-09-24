ff81dfb1e2f6045d4209853991b4e5dbe79d475e
Marko Tikkanen <martikkanen@outlook.com>
2023-09-24 15:00:19 +0300

    .

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -3,10 +3,10 @@ use match_numbers::{_generate_vec, find_pair, find_pair_iter, find_pair_map};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let my_vec = black_box(_generate_vec(100_000, 5643, 967, 40));
+    let my_vec = black_box(_generate_vec(1_000_000, 5643, 967, 40));
     let target: i32 = 6610;
 
-    c.bench_function("find_pair", |b| b.iter(|| find_pair(&my_vec, &target)));
+    //c.bench_function("find_pair", |b| b.iter(|| find_pair(&my_vec, &target)));
     c.bench_function("find_pair_iter", |b| {
         b.iter(|| find_pair_iter(&my_vec, &target))
     });
